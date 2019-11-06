bb6ed4dc635befd79cd73abd5c60890191ad2b8e
Marcelo Ecker <marcelo.ecker@bry.com.br>
2019-11-06 17:25:23 -0300

    aula 109 validação cep

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -54,7 +54,7 @@ export class DataFormComponent implements OnInit {
       email: [ null, [ Validators.required, Validators.email ] ],
 
       endereco: this.formBuilder.group( {
-        cep: [ null, Validators.required ],
+        cep: [ null, [ Validators.required, FormValidations.cepValidator ] ],
         numero: [ null, Validators.required ],
         complemento: [ null ],
         rua: [ null, Validators.required ],
@@ -130,6 +130,13 @@ export class DataFormComponent implements OnInit {
   verificaValidTouched( campo: string ) {
     return (
       !this.formulario.get( campo ).valid &&
+      ( this.formulario.get( campo ).touched || this.formulario.get( campo ).dirty )
+    );
+  }
+
+  verificaRequired( campo: string ) {
+    return (
+      this.formulario.get( campo ).hasError( 'required' ) &&
       ( this.formulario.get( campo ).touched || this.formulario.get( campo ).dirty )
     );
   }
