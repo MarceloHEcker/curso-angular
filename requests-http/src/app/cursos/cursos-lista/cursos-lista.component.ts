39c830bd9dd99b745aa26e70f3483d34b8c70edf
Marcelo Ecker <marcelo.ecker@bry.com.br>
2019-11-14 18:03:11 -0300

    aula 124 capturando erros

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CursosService } from '../cursos.service';
 import { Curso } from './../curso';
-import { Observable } from 'rxjs';
+import { Observable, empty, of, Subject } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 
 @Component( {
   selector: 'app-cursos-lista',
@@ -12,11 +13,31 @@ import { Observable } from 'rxjs';
 export class CursosListaComponent implements OnInit {
 
   cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor( private service: CursosService ) { }
 
   ngOnInit() {
-    this.cursos$ = this.service.list();
+    this.onRefresh();
+  }
+
+  onRefresh() {
+    this.cursos$ = this.service.list().pipe(
+      // map(), 
+      // tap(), 
+      // switchMap(),
+      catchError( error => {
+        console.error( error );
+        this.error$.next( true );
+        return empty();
+      } )
+    );
+
+    this.service.list().pipe( catchError( error => empty() ) ).subscribe(
+      dados => {
+        console.log( dados );
+      }
+    );
   }
 
 }
