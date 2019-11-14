dff364e3081112c610553ce6f897f351b2c39b9e
Marcelo Ecker <marcelo.ecker@bry.com.br>
2019-11-14 19:23:24 -0300

    aula 128 finalizando criação do curso

--- requests-http/src/app/cursos/cursos.service.ts
+++ requests-http/src/app/cursos/cursos.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Curso } from './curso';
-import { tap, delay } from 'rxjs/operators';
+import { tap, delay, take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
 
@@ -11,12 +11,18 @@ import { environment } from 'src/environments/environment';
 } )
 export class CursosService {
 
+
   private readonly API = `${ environment.API }cursos`;
 
   constructor( private http: HttpClient ) { }
 
   list() {
     return this.http.get<Curso[]>( this.API ).pipe( delay( 2000 ), tap( console.log ) );
-
   }
+
+  create( curso ) {
+    return this.http.post( this.API, curso ).pipe( take( 1 ) );
+  }
+
+
 }
