8090c79f2054d4fe44c7d5ce6948fe2f29d56b77
Marcelo Ecker <marcelo.ecker@bry.com.br>
2020-03-02 17:39:58 -0300

    aula 142 tela de pesquisa reativa

--- requests-http/src/app/app.module.ts
+++ requests-http/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { SharedModule } from './shared/shared.module';
 import { UploadFileComponent } from './upload-file/upload-file.component';
+import { ReactiveSearchComponent } from './reactive-search/reactive-search.component';
 
 @NgModule( {
-  declarations: [ AppComponent, UploadFileComponent ],
+  declarations: [ AppComponent, UploadFileComponent, ReactiveSearchComponent ],
   imports: [ BrowserModule, AppRoutingModule, HttpClientModule, ModalModule.forRoot(), SharedModule ],
   providers: [],
   bootstrap: [ AppComponent ]
